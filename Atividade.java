31609c75d73660fb857a2cbb9d56a7c271566dec
Felipe Schmitz <104650985+devmitz@users.noreply.github.com>
2023-03-13 13:16:26 -0300

    melhorias e correção de bugs

--- Atividade.java
+++ Atividade.java
@@ -1,48 +1,75 @@
 import javax.swing.JOptionPane;
 public class Atividade {
 	public static void main(String[] args) {
-		int variavel_um, variavel_dois;
-		String msg_op = "Digite o valor da operação \n (+) = Soma \n (-) = Subtração \n (/) = Divisão";
+		//inicialização das variaveis.
+		int variavelUm, variavelDois;
+		String mensagemOperacao = "Digite o valor da operação \n (+) = Soma \n (-) = Subtração \n (/) = Divisão \n (*) = Multiplicação";
+		boolean continuar = true;
+		boolean operadorValido = true;
 		
-		String continuar = "sim";
-		boolean operador_valido = true;
-		
-		while(continuar.equals("sim")) {
+		while(continuar) {
 			//Começando a perguntar ao usuario os números e a operação que deseja fazer.
-			while(operador_valido) {
-				variavel_um = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número: "));
-				String operacao = JOptionPane.showInputDialog(msg_op);
-				variavel_dois = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número: "));
-				//resultados.
-				int resultado_soma = variavel_um + variavel_dois;
-				int resultado_subtracao = variavel_um - variavel_dois;
-				double resultado_divisao = variavel_um / variavel_dois;
+			while(operadorValido) {
+				//entrada de dados.
+				variavelUm = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número: "));
+				String operacao = JOptionPane.showInputDialog(mensagemOperacao);
+				variavelDois = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número: "));
 				
 				//Se algum número digitado for = 0, dara a mensagem de operador invalido.
-				if (variavel_um != 0 && variavel_dois != 0) {
-					if(operacao.equals("+")) {
-						JOptionPane.showMessageDialog(null, "A soma da operação foi de : " + resultado_soma);
-					}else if(operacao.equals("-")) {
-						JOptionPane.showMessageDialog(null, "A subtração da operação foi de : " + resultado_subtracao);
-					}else if(operacao.equals("/")) {
-						JOptionPane.showMessageDialog(null, "A divisão da operação foi de : " + resultado_divisao);
-					}else {
-						JOptionPane.showMessageDialog(null, "Operador inválida, digite novamente.");
-						operador_valido = false;
+				if (variavelUm != 0 && variavelDois != 0) {
+
+					switch (operacao) {
+						case "+":
+							int resultadoSoma = variavelUm + variavelDois;
+							JOptionPane.showMessageDialog(null, "A soma da operação foi de : " + resultadoSoma);
+							break;
+						
+						case "-":
+							int resultadoSubtracao = variavelUm - variavelDois;
+							JOptionPane.showMessageDialog(null, "A subtração da operação foi de : " + resultadoSubtracao);
+							break;
+						
+						case "/":
+							double resultadoDivisao = variavelUm / variavelDois;
+							JOptionPane.showMessageDialog(null, "A divisão da operação foi de : " + resultadoDivisao);
+							break;
+
+						case "*":
+							double resultadoMultiplicacao = variavelUm * variavelDois;
+							JOptionPane.showMessageDialog(null, "A divisão da operação foi de : " + resultadoMultiplicacao);
+							break;
+
+						default:
+							JOptionPane.showMessageDialog(null, "Operador inválida, digite novamente.");
+							operadorValido = false;
+							break;
 					}
 				}else{
 					JOptionPane.showMessageDialog(null, "Número invalido, ele deve ser diferente de 0.");
 				}
 				
-				//Aqui ele ira perguntar ao usuario se ele deseja continuar ou deseja encerrar o programa.
-				String perg_continuar = JOptionPane.showInputDialog("Você deseja continuar calculando? \n Escreva: (sim) ou (não)");
-				if(perg_continuar.equals("não")){
-					continuar = "não";
-					break;
-				}else if(perg_continuar.equals("sim")){
-					continuar = "sim";
-				}else {
-					JOptionPane.showMessageDialog(null, "Conteudo digitado para continuar, inválido.");
+				Boolean continuaLoop = true;
+
+				while(continuaLoop) {
+					//Aqui ele ira perguntar ao usuario se ele deseja continuar ou deseja encerrar o programa.
+					String pergContinua = JOptionPane.showInputDialog("Você deseja continuar calculando? \n Escreva: (s) ou (n)");
+
+					switch (pergContinua) {
+						case "s":
+							continuar = true;
+							continuaLoop = false;
+							break;
+						
+						case "n":
+							continuar = false;
+							continuaLoop = false;
+							operadorValido = false;
+							break;
+						
+						default:
+							JOptionPane.showMessageDialog(null, "Conteudo digitado para continuar, inválido.");
+							break;
+					}
 				}
 			}
 		}
--- AulaCalculadora.java
+++ AulaCalculadora.java
@@ -1,4 +1,4 @@
-package com.mycompany.aulacalculadora;
+//package com.mycompany.aulacalculadora;
 
 import java.util.Scanner;
 
